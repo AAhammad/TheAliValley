e3b7f89e1682535525298dc6cdcf310b9413c7da
AAhammad <abeg42096@gmail.com>
2020-08-17 13:17:04 +0600

    Product Type View and bootstrap add

--- TheAliValley/Areas/Admin/Controllers/ProductTypesController.cs
+++ TheAliValley/Areas/Admin/Controllers/ProductTypesController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheAliVally.Data;
 
 namespace TheAliValley.Areas.Admin.Controllers
-{
+{  
+    [Area("Admin")]
     public class ProductTypesController : Controller
     {
+        private ApplicationDbContext _db;
+
+        public ProductTypesController (ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_db.ProductTypes.ToList());
         }
     }
 }
